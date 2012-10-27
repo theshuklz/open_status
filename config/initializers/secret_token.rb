913e2d0665ed97c0bccdf4f5efda88331d94cdd9
Rishikesh Shukla <rishi1160@gmail.com>
2012-10-27 13:14:36 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenStatus::Application.config.secret_token = '01f4c171db76306a35e6c8332b3f56b5db6af6abf3625d27e23dcad9b53d2f50e93b271e972f3c5f6fad47874a25ed029170678cf7325e9e873f8b39bc971cb5'
